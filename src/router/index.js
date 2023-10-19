44a0b09295f9b92ef7c8c79e2dfcb33b32188d89
HETTIARACHCHI H.A.D.A.S <asindushashan99@gmail.com>
2023-10-19 15:26:09 +0530

    Add New Routings
    asindu

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
-// impor
+// import componets
 import { createRouter, createWebHistory } from 'vue-router';
 import MainContent from "@/components/MainContent.vue";
 import Page3Dashboard from '@/components/Page3Dashboard.vue';
 import Page4Wallet from '@/components/Page4Wallet.vue';
 import Storage from '@/components/Storage.vue'
 import Page5AccessKey from '@/components/Page5AccessKey.vue';
+<<<<<<< Updated upstream
 import MobileVerifyComp from '@/components/MobileVerifyComp.vue'
 import Billing from '@/components/Billing.vue';
+=======
+import MobileVerifyComp from '@/components/MobileVerify/MobileVerifyComp.vue'
+
+import VerifyComp from '@/components/MobileVerify/VerifyComp.vue';
+import NumberVerifyComp from '@/components/MobileVerify/NumberVerifyComp.vue';
+>>>>>>> Stashed changes
 
 
 
@@ -43,6 +50,16 @@ const routes = [
     {
       path : '/Mobile-Verfication',
       component : MobileVerifyComp,
+      children : [
+        {
+          path : '',
+          component : VerifyComp,
+        },
+        {
+          path : '/Mobile-Verfication/Number-Verify',
+          component : NumberVerifyComp,
+        }
+      ]
     }
     
   ]
