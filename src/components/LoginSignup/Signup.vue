7b9b38c71b2be037b07e3ada52a20786ad70f751
IT21152832 <it21152832@my.sliit.lk>
2023-10-21 22:45:16 +0530

    add new components and some changes
    dulanjani

---
+++ src/components/LoginSignup/Signup.vue
@@ -0,0 +1,27 @@
+<template>
+    <!-- signup -->
+<div class="h-screen">
+    <div class="flex lg:flex-row shadow:xl lg:p-12">
+        <!-- left-side  -->
+        <div class="lg:h-[600px] bg-[linear-gradient(to_right,rgba(240,245,254,1),rgba(240,245,254,1))] lg:rounded-l-xl">
+            <!-- image  -->
+            <div class="">
+                <img src="../../img/images/signup-img2.png" alt="" class="lg:mt-20 lg:mx-20 lg:transform  lg:scale-[1.1]">
+
+            </div>
+
+        </div>
+
+        <div class="lg:h-[600px] bg-white lg:rounded-r-xl">
+           <router-view></router-view>
+        </div>
+
+    </div>  
+</div>
+</template>
+
+<script>
+export default {
+    name : 'Signup'
+}
+</script>
