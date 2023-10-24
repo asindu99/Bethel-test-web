ef8d31a1e9cceb6333b36c3b14d486dc25d9a266
HETTIARACHCHI H.A.D.A.S <asindushashan99@gmail.com>
2023-10-24 16:05:29 +0530

    pushed real

---
+++ src/components/LoginSignup2/FormContent/Signup-second.vue
@@ -0,0 +1,61 @@
+<template>
+    <!-- personal info form cont... -->
+
+    <div class="flex justify-between w-full mt-4">
+                    <!-- first name -->
+                    <div class="flex flex-col">
+                        <label for="" class="text-[14px]">First Name :</label>
+                        <input type="text" class=" bg-transparent p-1 border-b-2 border-[#29379384] outline-none">
+                    </div>
+
+                    <!-- last name -->
+                    <div class="flex flex-col">
+                        <label for="" class="text-[14px]">Last Name :</label>
+                        <input type="text" class=" bg-transparent p-1 border-b-2 border-[#29379384] outline-none">
+                    </div>
+                </div>
+
+
+                <div class="flex justify-between w-full mt-4">
+
+                    <!-- Select country name -->
+                    <div class="flex flex-col">
+                        <label for="" class="text-[14px]">Select Country :</label>
+                        <input type="text" class=" bg-transparent p-1 border-b-2 border-[#29379384] outline-none">
+                    </div>
+
+                    <!-- last name -->
+                    <div class="flex flex-col">
+                        <label for="" class="text-[14px]">Country Code :</label>
+                        <input type="text" class=" bg-transparent p-1 border-b-2 border-[#29379384] outline-none">
+                    </div>
+
+                </div>
+
+
+                <div class="flex justify-between w-full mt-4">
+
+                    <!-- first name -->
+                    <div class="flex flex-col">
+                        <label for="" class="text-[14px]">Mobile Number :</label>
+                        <input type="number" class=" bg-transparent p-1 border-b-2 border-[#29379384] outline-none">
+                    </div>
+
+                </div>
+
+                <div class="flex justify-between w-full mt-4">
+
+                    <!-- tos name -->
+                    <div class="flex items-center gap-2">
+                        <input type="checkbox" class="scale-[1.3]">
+                        <label for="" class="text-[12px]">I agree to the terms of Bethel Platform cloud terms of service , General terms of service and Privacy Policy </label>
+                    </div>
+
+                </div>
+</template>
+
+<script>
+export default{
+    name : 'Signup-second'
+}
+</script>
--- src/components/LoginSignup2/LoginComp2.vue
+++ src/components/LoginSignup2/LoginComp2.vue
@@ -3,7 +3,7 @@
 
         <!-- main container  -->
         <div class="w-full lg:h-screen flex justify-center items-center 
-        lg:p-20 md:py-[0px] md:h-screen sm:p-10 min-[320px]:p-3 ">
+        lg:p-20 md:py-[10px] md:h-screen sm:p-10 min-[320px]:p-3 ">
 
             <div class="lg:flex-row md:flex sm:flex-col-reverse min-[320px]:flex-col
              rounded-xl shadow-lg bg-white w-full">
@@ -11,80 +11,20 @@
 
                 <img class="w-[600px] lg:hidden md:hidden sm:hidden min-[320px]:flex sm:w-[300px]" src="../../img/images/LoginSignup-page-images/BG-login.png" alt="">
 
-                <div class="flex flex-1 px-10 lg:py-20 md:py-5 lg:w-[600px] 
+                <div class="flex flex-1 px-10 lg:py-20 md:py-5 lg:w-[600px] lg:h-[700px] md:w-full
                 min-[320px]:py-10">
-                    <!-- content div -->
-                    <div class="w-full h-full lg:px-10 justify-center items-center flex flex-col">
 
-                        <!-- full div bethel and upper content -->
-                        <div class="flex py-2 lg:items-start md:items-center w-full
-                        gap-1 ">
+                <!---------------------- router view goes here------------------- -->
+                <!-- <LoginContent/> -->
+                <SignupContent/>
 
-                            <!-- bethel logo div -->
-                            <div class="flex md:items-center lg:justify-start md:justify-center w-full gap-1">
-                                <img class="w-[30px]" src="../../img/logos/bethellogo.png" alt="">
-                                <h3 class="text-[#293793] font-medium ">BETHEL</h3>
-                            </div>
-
-                            <!--  -->
-                            <div>
-                                
-                            </div>
-                        </div>
-                        <!-- end of the upper section -->
-
-
-
-                        <!-- middle section -->
-                        <div class="flex flex-col md:items-center lg:items-start lg:justify-start w-full">
-
-                            <!-- introdution section -->
-                            <div class="flex flex-col mt-2 lg:text-left md:text-center">
-                                <h1 class="lg:text-[25px] md:text-[25px] sm:text-[25px] min-[320px]:text-[25px] text-[#293793]">Welcome to the BETHEL</h1>
-                                <h3 class="lg:text-[14px] md:text-[14px] sm:text-[14px] min-[320px]:text-[14px] text-[#757784]">Please sign-in to your account and start the adventure</h3>
-                            </div>
-
-                            <!-- form section -->
-                            <div class="mt-5 w-full">
-                                <form action="">
-                                    <div class="flex flex-col">
-                                    <label for="" class="text-[14px] mb-1">Email :</label>
-                                    <input type="text" class=" bg-transparent p-1 border-b-2 border-[#29379384] outline-none">
-                                    </div>
-
-                                    <div class="flex flex-col w-full mt-4">
-                                    <label for="" class="text-[14px]">Password :</label>
-                                    <input type="password" class=" bg-transparent p-1 border-b-2 border-[#29379384] outline-none">
-                                    <button class="w-full text-left  mt-2"><h3 class="text-[12px] text-[#293793]">Forget password?</h3></button>
-                                    </div>
-
-                                    <!-- sign in button -->
-                                    <div class="w-full mt-4">
-                                        <button class="w-full p-2 rounded-lg bg-[#293793] text-white">Sign In</button>
-                                    </div>
-                                    
-                                </form>
-
-                                <!-- under div -->
-                                <div class="w-full text-center mt-2">
-                                    <h3 class="text-[13px] text-[#757784]">New on our platform ?
-                                        <button class="text-[#293793]">Create an Account</button>
-                                    </h3>
-                                </div>
-                            </div>
-
-                        </div>
-                        <!-- end of the middle section -->
-                        
-                    </div>
-                    <!-- end of the content -->
                 </div>
 
 
 
                 <!-- image side right -->
                 <div class="lg:flex md:flex sm:flex min-[320px]:hidden lg:flex-1 justify-center items-center overflow-hidden w-full">
-                    <img class="lg:w-[800px] md:w-[500px] sm:w-[300px]" src="../../img/images/LoginSignup-page-images/BG-login.png" alt="">
+                    <img class="lg:w-[800px] md:w-[400px] sm:w-[300px] md:pt-4" src="../../img/images/LoginSignup-page-images/BG-login.png" alt="">
                 </div> 
             </div>
             
@@ -96,8 +36,16 @@
 </template>
 
 <script>
+import LoginContent from '@/components/LoginSignup2/FormContent/LoginContent.vue';
+import SignupContent from '@/components/LoginSignup2/FormContent/SignupContent.vue';
+
+
 export default {
-    name : 'LoginComp'
+    name : 'LoginComp',
+    components : {
+        LoginContent,
+        SignupContent,
+    }
 }
 
 </script>
