524c7143bd99c7a1cae699da9c36355aee1e6b2e
HETTIARACHCHI H.A.D.A.S <asindushashan99@gmail.com>
2023-10-25 17:06:16 +0530

    dahs

--- src/components/LoginSignup2/Mobile-verify/MobileVerify.vue
+++ src/components/LoginSignup2/Mobile-verify/MobileVerify.vue
@@ -27,7 +27,8 @@ lg:p-20 md:py-[10px] md:h-screen sm:p-10 min-[320px]:p-3 ">
             <div class="flex flex-col w-full items-center justify-center gap-4 mt-10">
 
                 <h3 class="min-[320px]:text-center lg:text-[14px] md:text-[14px] sm:text-[14px] min-[320px]:text-[15px] text-center text-[#293793]">Enter the OTP code sent to your Mobile :</h3>
-                <input type="number" class="text-[20px] tracking-tighter bg-transparent p-1 border-b-2 border-[#29379384] outline-none">
+                <input type="number" class="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none
+                text-[20px] tracking-tighter bg-transparent p-1 border-b-2 border-[#29379384] outline-none">
                 <router-link to="/"><button class="p-2 bg-[#293793] rounded-lg px-10 text-white border-[1px] hover:bg-white hover:text-[#293793] hover:border-[1px] hover:border-[#293793] transition-all ease-in-out ">Verify</button></router-link>
             </div>
         
