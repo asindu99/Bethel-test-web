55e1703c2df44b56a06ecdebc57bc35a18ea1868
IT21152832 <it21152832@my.sliit.lk>
2023-10-30 14:47:47 +0530

    changed

--- src/components/Profile/ChangePassword.vue
+++ src/components/Profile/ChangePassword.vue
@@ -1,18 +1,18 @@
 <template>
-    <!-- personal info form cont... -->
+    <!-- change password info form cont... -->
 
 <div class="px-8 lg:py-14 min-[320px]:py-4 lg:mt-[-50px]">
-    <!-- personal information div -->
+    <!-- change passowrd information div -->
     <div class="flex flex-col w-full mt-10 lg:justify-start min-[320px]:items-center">
         <h1 class="text-[20px] text-[#293793] font-bold">Change Password</h1>
     </div>
 
-    <hr class="w-full lg:mt-8 md:mt-2 sm:mt-2 min-[320px]:mt-2">
+    <hr class="w-full lg:mt-10 md:mt-2 sm:mt-2 min-[320px]:mt-2">
 
     <div class="lg:flex-row md:flex-row sm:flex min-[320px]:flex-col justify-between w-full mt-8">
 
 
-        <!-- first name -->
+        <!-- old password -->
         <div class="flex flex-col mr-2">
             <label for="" class="text-[14px]">Old Password :</label>
             <input type="password" class="lg:w-[460px] md:w-[240px] text-[#757784] bg-transparent p-1 border-b-2 border-[#29379384]">
@@ -24,13 +24,13 @@
 
     <div class="flex justify-between w-full mt-4 lg:flex-row md:flex-col sm:flex-col min-[320px]:flex-col">
 
-        <!-- last name -->
+        <!-- new password -->
         <div class="flex flex-col min-[320px]:mt-0 lg:mt-0 md:mt-0 sm:mt-0 mr-2">
             <label for="" class="text-[14px]">New Password :</label>
             <input type="password" class="lg:w-[235px] text-[#757784] bg-transparent p-1 border-b-2 border-[#29379384] outline-1 ">
         </div>
-        <!-- first name -->
-        <div class="flex flex-col min-[320px]:mt-2">
+        <!-- confirm password -->
+        <div class="flex flex-col min-[320px]:mt-2 lg:mt-0">
             <label for="" class="text-[14px]">Confirm Password :</label>
             <input type="password" class="text-[#757784] bg-transparent p-1 border-b-2 border-[#29379384] w-[235px] 
             [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none ">
