499b2dbcc880fd79c0ba75c77c003caa9c1c9e0a
HETTIARACHCHI H.A.D.A.S <asindushashan99@gmail.com>
2023-11-03 11:31:14 +0530

    done

--- src/stores/AuthUser.js
+++ src/stores/AuthUser.js
@@ -1,12 +1,15 @@
-import {defineStore} from 'pinia';
+import {defineStore} from 'pinia'
+import router from '@/router/index'
 
 export const authUser = defineStore('authUser', {
     state : () => ({
         authUserDetailsArr : [],
+        authMobileOTP : [],
 
     }),
 
     actions : {
+        // add signup form data into json --> componet --> signup-third
         async postAuthUserData(values){
             this.authUserDetailsArr.push(values)
 
@@ -18,6 +21,28 @@ export const authUser = defineStore('authUser', {
 
             if(res.error){
                 console.log(res.error)
+            }
+            else{
+                router.push('/mobile-verify')
+            }
+        },
+
+        // add mobile OTP into the json --> component --> mobileVerify
+        async postMobileOTP(values){
+            this.authMobileOTP.push(values)
+
+            const res = await fetch('http://localhost:3000/OTP' , {
+                method : 'POST',
+                body : JSON.stringify(values),
+                headers : {'Content-Type' : 'application/json'}
+
+            });
+
+            if(res.error){
+                console.log(res.error)
+            }
+            else{
+                router.push('/')
             }
         }
     }
