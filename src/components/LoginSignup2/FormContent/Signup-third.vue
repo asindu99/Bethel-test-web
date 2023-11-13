2db558bc1bb1afc56dd47386f85b7e4ef8fd390b
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2023-11-13 16:21:12 +0530

    cdone

--- src/components/LoginSignup2/FormContent/Signup-third.vue
+++ src/components/LoginSignup2/FormContent/Signup-third.vue
@@ -6,44 +6,49 @@
                 </div>
 
                 <hr class="w-full mt-2">
-        <VeeForm @submit="onReg" :validation-schema="signupSchema">
+        <VeeForm @submit="onReg" :validation-schema="signupSchema" class="">
             <!-- email div -->
             
-                <div class="flex w-full justify-between mt-8
-                    lg:flex-row md:flex-row sm:flex min-[320px]:flex-col lg:gap-8">
-                    <!-- password div -->
-                    <div class="flex flex-col relative">
-                        <label for="" class="text-[14px]">Email :</label>
-                        <VeeField  name="email" type="text" class=" bg-transparent p-1 border-2 border-[#29379384] outline-none rounded-md
-                        lg:gap-4 md:gap-4 sm:gap-4 min-[320px]:gap-4 lg:w-[235px] md:w-[238px] min-[320px]:w-full w-[340px]"/>
+                <div class="flex w-full justify-center gap-y-12 mt-8 lg:flex-col md:flex-col sm:flex-col min-[320px]:flex-col  lg:px-10 md:px-40 sm:px-10 min-[320px]:px-5">
+                    <!-- email div -->
+                    <div class="relative flex flex-col">
+                        <vee-field autocomplete="off" id="email" name="email" type="email" class="w-full h-10 text-gray-900 placeholder-transparent border-b-2 border-sidebarBG bg-blue-50 focus:outline-none focus:borer-rose-600 on:focus:bg-white" placeholder="email" />
+							<label for="email" class="absolute left-0 -top-6 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5  peer-focus:text-sm">Email :</label>
 
                         <ErrorMessage name="email" class="absolute text-[12px] text-red-500 bottom-[-20px]"/>
                     </div>
 
-                    <!-- confirm password div -->
+                    <!-- username password div -->
                     <div class="flex flex-col relative lg:mt-0 md:mt-0 sm:mt-4 min-[320px]:mt-5">
-                        <label for="" class="text-[14px]">User Name :</label>
-                        <VeeField  name="username" type="text" class=" bg-transparent p-1 border-2 border-[#29379384] outline-none rounded-md lg:w-[235px] md:w-[238px] min-[320px]:w-full w-[340px]"/>
-                        <ErrorMessage name="name" class="absolute text-[12px] text-red-500 bottom-[-20px]"/>
+                        
+                        <!-- <label for="" class="text-[14px]">User Name :</label>
+                        <VeeField  name="username" type="text" class=" bg-transparent p-1 border-2 border-[#29379384] outline-none rounded-md lg:w-[235px] md:w-[238px] min-[320px]:w-full w-[340px]"/> -->
+                        
+                        <vee-field autocomplete="off" id="username" name="username" type="text" class="w-full h-10 text-gray-900 placeholder-transparent border-b-2 border-sidebarBG bg-blue-50 focus:outline-none focus:borer-rose-600 on:focus:bg-white" placeholder="username" />
+							<label for="username" class="absolute left-0 -top-6 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5  peer-focus:text-sm">User Name :</label>
                     </div>
 
                 </div>
 
                 <!-- password and confirm password section -->
-                <div class="flex w-full justify-between gap-2 mt-4
-                lg:flex-row md:flex-row sm:flex min-[320px]:flex-col lg:gap-8">
+                <div class="flex w-full justify-center gap-y-12 mt-4 lg:flex-col md:flex-col sm:flex-col min-[320px]:flex-col  lg:px-10 md:px-40 sm:px-10 min-[320px] pt-8 :px-5">
                     <!-- password div -->
-                    <div class="flex flex-col relative">
-                        <label for="" class="text-[14px]">Password :</label>
-                        <VeeField  name="password" type="password" class=" bg-transparent p-1 border-2 border-[#29379384] outline-none rounded-md lg:w-[235px] md:w-[238px] min-[320px]:w-full w-[340px]"/>
+                    <div class="relative flex flex-col">
+                        <!-- <label for="" class="text-[14px]">Password :</label>
+                        <VeeField  name="password" type="password" class=" bg-transparent p-1 border-2 border-[#29379384] outline-none rounded-md lg:w-[235px] md:w-[238px] min-[320px]:w-full w-[340px]"/> -->
 
+                        <vee-field autocomplete="off" id="password" name="password" type="password" class="w-full h-10 text-gray-900 placeholder-transparent border-b-2 border-sidebarBG bg-blue-50 focus:outline-none focus:borer-rose-600 on:focus:bg-white" placeholder="password" />
+							<label for="password" class="absolute left-0 -top-6 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5  peer-focus:text-sm">Password :</label>
                         <ErrorMessage name="password" class="absolute text-[12px] text-red-500 bottom-[-20px]"/>
                     </div>
 
                     <!-- confirm password div -->
                     <div class="flex flex-col relative lg:mt-0 md:mt-0 sm:mt-4 min-[320px]:mt-4">
-                        <label for="" class="text-[14px]">Confirm Password :</label>
-                        <VeeField  name="confirm_password" type="password" class=" bg-transparent p-1 border-2 border-[#29379384] outline-none rounded-md lg:w-[235px] md:w-[238px] min-[320px]:w-full w-[340px]"/>
+                        <!-- <label for="" class="text-[14px]">Confirm Password :</label>
+                        <VeeField  name="confirm_password" type="password" class=" bg-transparent p-1 border-2 border-[#29379384] outline-none rounded-md lg:w-[235px] md:w-[238px] min-[320px]:w-full w-[340px]"/> -->
+                        
+                        <vee-field autocomplete="off" id="confirm_password" name="confirm_password" type="password" class="w-full h-10 text-gray-900 placeholder-transparent border-b-2 border-sidebarBG bg-blue-50 focus:outline-none focus:borer-rose-600 on:focus:bg-white" placeholder="confirm_password" />
+							<label for="confirm_password" class="absolute left-0 -top-6 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5  peer-focus:text-sm">Confirm Password :</label>
                         <ErrorMessage name="confirm_password" class="absolute text-[12px] text-red-500 bottom-[-20px]"/>
                     </div>
 
@@ -59,74 +64,10 @@
                     </router-link>
                 </div> -->
 
-                
-
-                <div class="lg:flex-row md:flex-row sm:flex min-[320px]:flex-col justify-between w-full mt-4 lg:gap-8">
-                    <!-- first name -->
-                    <div class="flex flex-col relative">
-                        <label for="" class="text-[14px]">First Name :</label>
-                        <VeeField name="FirstName" type="text" class="lg:w-[235px] md:w-[240px] text-[#757784] bg-transparent p-1 border-2 border-[#29379384] rounded-md lg:w-[235px] md:w-[238px] min-[320px]:w-full w-[340px]"/>
-                        <ErrorMessage name="FirstName" class="absolute text-[12px] text-red-500 bottom-[-20px]"/>
-
-                    </div>
-
-                    <!-- last name -->
-                    <div class="flex flex-col min-[320px]:mt-4 lg:mt-0 md:mt-0 sm:mt-4 relative">
-                        <label for="" class="text-[14px]">Last Name :</label>
-                        <VeeField name="LastName" type="text" class="lg:w-[235px] text-[#757784] bg-transparent p-1 border-2 border-[#29379384] outline-1 rounded-md lg:w-[235px] md:w-[238px] min-[320px]:w-full w-[340px]"/>
-                        <ErrorMessage name="LastName" class="absolute text-[12px] text-red-500 bottom-[-20px]"/>
-
-                    </div>
-                </div>
-
-
-                <div class="justify-between w-full mt-4 lg:flex-row md:flex-row sm:flex min-[320px]:flex-col lg:gap-8">
-
-                    <!-- Select country name -->
-                    <div  class="flex flex-col">
-                        <label for="" class="text-[14px]">Select Country :</label>
-                        <!-- <input type="text" class=" bg-transparent p-1 border-b-2 border-[#29379384] outline-none"> -->
-
-                        <vee-field as="select" name="country" v-model="selected" class="text-[#757784] bg-transparent p-2 border-2 border-[#29379384]  lg:w-[235px] md:w-[238px] min-[320px]:w-full w-[340px] rounded-md">
-                            <option v-for="country in countryList" :value="country.Name" class="w-[200px] bg-transparent">
-                                {{ country.Name }}
-                            </option>
-                        </vee-field>
-
-                        
-                    </div>
-
-                    <!-- last name -->
-                    <div  class="flex flex-col min-[320px]:mt-4 lg:mt-0 md:mt-0 sm:mt-4">
-                        <label for="" class="text-[14px]">Country Code :</label>
-
-                        <VeeField as="select"  name="code" class="text-[#757784] bg-transparent p-2 border-2 border-[#29379384]  lg:w-[235px] md:w-[238px] min-[320px]:w-full w-[340px] rounded-md">
-                            <option v-for="code in countryList" :value="code.MobileCode" class="w-[200px] bg-transparent">
-                                {{ code.MobileCode }}
-                            </option>
-                        </VeeField>
-                        <!-- <input type="text" class="text-[#757784] bg-transparent p-1 border-b-2 border-[#29379384] outline-none"> -->
-                    </div>
-
-                </div>
-
-
-                <div class="flex justify-between w-full mt-2">
-
-                    <!-- first name -->
-                    <div class="flex flex-col relative mt-2 w-full">
-                        <label for="" class="text-[14px]">Mobile Number :</label>
-                        <VeeField name="mobile" type="Number" class="text-[#757784] bg-transparent p-1 border-2 border-[#29379384]
-                        [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none rounded-md"/>
-                        <ErrorMessage name="mobile" class="absolute text-[12px] text-red-500 bottom-[-20px]"/>
-                    </div>
-
-                </div>
-
-                <div class="flex flex-col justify-between w-full mt-2">
+                <div class="flex flex-col justify-between w-full mt-10 lg:px-10 md:px-40 sm:px-10 min-[320px]:px-5">
 
                     <!-- tos name -->
-                    <div class="flex items-center gap-2 mt-4 relative">
+                    <div class="relative flex items-center gap-2 mt-4">
                         <VeeField name="tos" type="checkbox" value="0" class="scale-[1.3]"/>
                         <ErrorMessage name="tos" class="absolute text-[12px] text-red-500 bottom-[-20px]"/>
                         <label for="" class="text-[12px]">I agree to the terms of Bethel Platform cloud terms of service , General terms of service and Privacy Policy </label>
@@ -137,7 +78,9 @@
                         <button type="submit" class="p-2 bg-sidebarBG rounded-lg px-10  text-white border-[1px] hover:bg-white hover:text-sidebarBG hover:border-[1px] hover:border-sidebarBG transition-all ease-in-out">Create Account</button>
                     </div>
                     <!-- </router-link> -->
-                </div>
+</div>
+                
+
                 
         </VeeForm>
                 
@@ -182,3 +125,24 @@ export default{
 }
 
 </script>
+
+<style scoped>
+
+input[type=text]:focus {
+  border: 2px solid #5669cc ;
+  border-radius: 10px;
+}
+
+input[type=password]:focus {
+  border: 2px solid #5669cc ;
+  border-radius: 10px;
+}
+
+input[type=email]:focus {
+  border: 2px solid #5669cc ;
+  border-radius: 10px;
+}
+
+
+
+</style>
